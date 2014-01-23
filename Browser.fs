e38b849617b50ff259833967ef03201de327c1d9
Taha Hachana <tahahachana@gmail.com>
2014-01-23 09:57:21 +0100

    Use chrome instead of firefox for dynamic charts

--- Browser.fs
+++ Browser.fs
@@ -4,12 +4,13 @@
 #r """.\packages\Selenium.WebDriver.2.39.0\lib\net40\WebDriver.dll"""
 #endif
 
+open OpenQA.Selenium.Chrome
 open OpenQA.Selenium.Firefox
 
 let size = System.Drawing.Size(700, 500)
 
 let start() =
-    let driver = new FirefoxDriver()
+    let driver = new ChromeDriver(__SOURCE_DIRECTORY__)
     let window = driver.Manage().Window
     window.Size <- size
-    driver  
+    driver
