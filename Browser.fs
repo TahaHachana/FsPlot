aab3a80609da706f8d420b2dcd66dd5bbc45ba58
Taha Hachana <tahahachana@gmail.com>
2014-02-07 11:10:04 +0100

    Fix locating Chrome's driver

--- Browser.fs
+++ Browser.fs
@@ -6,10 +6,20 @@
 
 open OpenQA.Selenium.Chrome
 open System.Drawing
+open System.IO
+open System.Reflection
+
+let private locateDriver() =
+    let solutionDir =
+        Assembly.GetExecutingAssembly().Location
+        |> fun x -> x.Remove(x.IndexOf("""\packages\"""))
+    Directory.GetFiles(solutionDir, "chromedriver.exe", SearchOption.AllDirectories).[0]
+    |> Directory.GetParent
+    |> fun x -> x.FullName
 
 let private size = Size(700, 500)
 
 let start() =
-    let driver = new ChromeDriver(__SOURCE_DIRECTORY__)
+    let driver = new ChromeDriver(locateDriver())
     driver.Manage().Window.Size <- size
     driver
