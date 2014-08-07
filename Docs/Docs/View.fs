8b58ca4a8d98686bf93d6b953a3bbdc26113ed1e
Taha Hachana <tahahachana@gmail.com>
2014-08-07 07:26:57 +0100

    Fix nav links

--- Docs/Docs/View.fs
+++ Docs/Docs/View.fs
@@ -10,8 +10,6 @@ let home =
 
         ]
 
-
-
 let googleCharts =
     Skin.withGoogleTemplate "Google Charts" <| fun ctx ->
         [
@@ -25,7 +23,7 @@ let highcharts =
         ]
 
 let chart title gistId =
-     Skin.withChartTemplate title <| fun ctx ->
+     Skin.withChartTemplate ("FsPlot · " + title) <| fun ctx ->
         [
             Div [Class "page-header"] -< [
                 H1 [Text title]
@@ -33,5 +31,5 @@ let chart title gistId =
             H2 [Text "Code"]
             Script [Src <| "https://gist.github.com/TahaHachana/" + gistId + ".js"]
             H2 [Text "Chart"]
-            IFrame [Src <| "iframe/" + gistId + ".html"; Id "chart-iframe"]
+            IFrame [Src <| "../iframe/" + gistId + ".html"; Id "chart-iframe"]
         ]
