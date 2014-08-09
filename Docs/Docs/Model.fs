2bb68b282fce516a61d279a3639d49debdf1eb60
Taha Hachana <tahahachana@gmail.com>
2014-08-09 08:45:47 +0100

    Docs back in master

---
+++ Docs/Docs/Model.fs
@@ -0,0 +1,12 @@
+﻿module HtmlApp.Model
+
+
+type Action =
+    | Home
+    | GoogleCharts
+    | Highcharts
+    | Chart of title * gistId
+
+and title = string
+
+and gistId = string
---
+++ Docs/Docs/View.fs
@@ -0,0 +1,35 @@
+﻿module HtmlApp.View
+
+open HtmlApp.Model
+open HtmlApp.Skin
+open IntelliFactory.Html
+
+let home =
+    Skin.withHomeTemplate "FsPlot" <| fun ctx ->
+        [
+
+        ]
+
+let googleCharts =
+    Skin.withGoogleTemplate "FsPlot · Google Charts Support" <| fun ctx ->
+        [
+        ]
+
+
+let highcharts =
+    Skin.withHighchartsTemplate "FsPlot · Highcharts Support" <| fun ctx ->
+        [
+
+        ]
+
+let chart title gistId =
+     Skin.withChartTemplate ("FsPlot · " + title) <| fun ctx ->
+        [
+            Div [Class "page-header"] -< [
+                H1 [Text title]
+            ]
+            H2 [Text "Code"]
+            Script [Src <| "https://gist.github.com/TahaHachana/" + gistId + ".js"]
+            H2 [Text "Chart"]
+            IFrame [Src <| "../iframe/" + gistId + ".html"; Id "chart-iframe"]
+        ]
