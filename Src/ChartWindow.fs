25a5479f433a94c1af60ec5482ebefecc24ccfd0
Taha Hachana <tahahachana@gmail.com>
2014-08-15 06:05:50 +0100

    Display highcharts charts in chrome

--- Src/ChartWindow.fs
+++ Src/ChartWindow.fs
@@ -1,29 +1,31 @@
 ﻿module internal FsPlot.ChartWindow
 
-#if INTERACTIVE
-#r "PresentationCore.dll"
-#r "PresentationFramework.dll"
-#r "WindowsBase.dll"
-#endif
+do ()
 
-open System
-open System.Windows
-open System.Windows.Controls
-open System.Windows.Media.Imaging
-
-/// Creates the bitmap frame used to set the chart's window icon.
-let private bitmapFrame =
-    let uriString = @"pack://application:,,,/FsPlot;component/ChartIcon.ico"
-    let iconUri = Uri(uriString, UriKind.RelativeOrAbsolute)
-    BitmapFrame.Create(iconUri)
-
-/// Displays a window containing a browser control.
-let show html =
-    let wnd = Window(Height = 700., Topmost = true, Width = 1000.)
-    wnd.Icon <- bitmapFrame
-    wnd.WindowStartupLocation <- WindowStartupLocation.CenterScreen 
-    let browser = new WebBrowser()
-    wnd.Content <- browser
-    wnd.Show()
-    wnd.Topmost <- false
-    wnd, browser
+//#if INTERACTIVE
+//#r "PresentationCore.dll"
+//#r "PresentationFramework.dll"
+//#r "WindowsBase.dll"
+//#endif
+//
+//open System
+//open System.Windows
+//open System.Windows.Controls
+//open System.Windows.Media.Imaging
+//
+///// Creates the bitmap frame used to set the chart's window icon.
+//let private bitmapFrame =
+//    let uriString = @"pack://application:,,,/FsPlot;component/ChartIcon.ico"
+//    let iconUri = Uri(uriString, UriKind.RelativeOrAbsolute)
+//    BitmapFrame.Create(iconUri)
+//
+///// Displays a window containing a browser control.
+//let show html =
+//    let wnd = Window(Height = 700., Topmost = true, Width = 1000.)
+//    wnd.Icon <- bitmapFrame
+//    wnd.WindowStartupLocation <- WindowStartupLocation.CenterScreen 
+//    let browser = new WebBrowser()
+//    wnd.Content <- browser
+//    wnd.Show()
+//    wnd.Topmost <- false
+//    wnd, browser
